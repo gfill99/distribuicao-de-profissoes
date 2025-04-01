186afc4792bdb3d5239faa3566494afffb4edaae
gfill99 <gabrielfil99@gmail.com>
2025-04-01 19:18:34 -0300

    gabriel - atualizando campos e arquivos

--- ingestao_de_dados/database/ETL_tabelas.sql
+++ ingestao_de_dados/database/ETL_tabelas.sql
@@ -5,29 +5,25 @@ CREATE TABLE IF NOT EXISTS tbl_estados_brasil (
 );
 
 CREATE TABLE IF NOT EXISTS tbl_advogados_brasil_2024(
-    estado VARCHAR (50) PRIMARY KEY,
-    id INTEGER REFERENCES tbl_estados_brasil(id), 
+    estado VARCHAR (50) PRIMARY KEY, 
     feminino INTEGER, 
     masculino INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS tbl_contadores_brasil_2024(
     estado VARCHAR (50) PRIMARY KEY,
-    id INTEGER REFERENCES tbl_estados_brasil(id), 
     feminino INTEGER, 
     masculino INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS tbl_engenheiros_brasil_2024(
     estado VARCHAR (50) PRIMARY KEY,
-    id INTEGER REFERENCES tbl_estados_brasil(id), 
     feminino INTEGER, 
     masculino INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS tbl_psicologos_brasil_2024(
-    estado VARCHAR (50) PRIMARY KEY,
-    id INTEGER REFERENCES tbl_estados_brasil(id), 
+    estado VARCHAR (50) PRIMARY KEY, 
     feminino INTEGER, 
     masculino INTEGER
 );
