d893543bbe64a4e5e1e38847e1cebaee660df3c4
André Leli <andreleli.dev@gmail.com>
2025-04-13 11:08:12 -0300

    subindo evolução de parametrização

--- analise_de_dados/utils/functions.py
+++ analise_de_dados/utils/functions.py
@@ -2,17 +2,22 @@ import duckdb as db
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import sys
 
-con = db.connect("../../ingestao_de_dados/database/db_distribuicao_profissoes.db")
+####################### MAPEANDO E CRIANDO CONEXÃO COM BANCO DE DADOS #######################
 
-# def contar_profissionais(genero_m,genero_f, profissao):    
-#     total_profissionais = con.execute(f"""
-#             SELECT nome_estado, ({genero_m.upper()}_{profissao} + {genero_f.upper()}_{profissao}) AS total_{profissao}
-#             FROM df_estados_profissoes
-#             ORDER BY total_{profissao} DESC
-#             LIMIT 5
-#         """).df()
-#     return total_profissionais
+sys.path.append(os.path.abspath('../..'))
+
+import configs as cf
+cf.mapeia_pastas()
+
+def criar_conexao_db():
+    con = db.connect("../../ingestao_de_dados/database/db_distribuicao_profissoes.db")
+    return con
+
+
+####################### FUNÇÕES QUE REALIZAM MANIPULAÇÃO DE DADOS #######################
+
 
 def contar_profissionais(con, genero_m, genero_f, profissao):    
     coluna_total = f"total_{profissao}"
@@ -21,41 +26,10 @@ def contar_profissionais(con, genero_m, genero_f, profissao):
                ({genero_m.upper()}_{profissao} + {genero_f.upper()}_{profissao}) AS {coluna_total}
         FROM df_estados_profissoes
         ORDER BY {coluna_total} DESC
-        LIMIT 5
+        LIMIT 7
     """).df()
     return df
 
-def plotar_maiores_estados_profissoes(con, profissoes, cores=None):
-    """
-    Gera subplots horizontais mostrando os 5 estados com mais profissionais por profissão.
-
-    Parâmetros:
-    - con: conexão ativa do DuckDB
-    - profissoes: lista de strings com os nomes das profissões (ex: ['advogados', 'contadores'])
-    - cores: lista de cores na mesma ordem da lista de profissões (opcional)
-    """
-    if cores is None:
-        cores = ['#7f8c8d'] * len(profissoes)
-
-    dados = []
-    for i, profissao in enumerate(profissoes):
-        df = contar_profissionais(con, 'M', 'F', profissao)
-        coluna_total = f'total_{profissao}'
-        cor = cores[i] if i < len(cores) else '#7f8c8d'
-        dados.append((df, coluna_total, profissao.capitalize(), cor))
-
-    fig, axs = plt.subplots(2, 2, figsize=(14, 10))
-    fig.suptitle('Top 5 estados com mais profissionais por profissão', fontsize=16)
-
-    for ax, (df, coluna, titulo, cor) in zip(axs.flat, dados):
-        ax.barh(df['nome_estado'], df[coluna], color=cor)
-        ax.set_title(titulo)
-        ax.invert_yaxis()
-        for i, v in enumerate(df[coluna]):
-            ax.text(v + max(df[coluna]) * 0.01, i, f'{int(v):,}'.replace(',', '.'), va='center', fontsize=9)
-
-    plt.tight_layout(rect=[0, 0, 1, 0.95])
-    plt.show()
 
 def distribuir_profissao_por_genero(con, genero, total_genero, nomes_femininos=False):
     if nomes_femininos:
@@ -84,9 +58,54 @@ def distribuir_profissao_por_genero(con, genero, total_genero, nomes_femininos=F
         FROM df_estados_profissoes
         GROUP BY nome_estado
         ORDER BY {total_genero} DESC
-        LIMIT 5
+        LIMIT 7
     """
     return con.execute(query).df()
+
+def ordena_query_profissão(con, genero, profissao):
+	profissao_genero = con.execute(f"""
+		SELECT nome_estado, {genero}_{profissao} AS total
+		FROM df_estados_profissoes
+		ORDER BY total DESC
+		LIMIT 7
+		""").df()
+	return profissao_genero
+
+
+####################### FUNÇÕES QUE REALIZAM CRIAÇÃO DE GRÁFICOS #######################
+
+def plotar_maiores_estados_profissoes(con, profissoes, cores=None):
+    """
+    Gera subplots horizontais mostrando os 5 estados com mais profissionais por profissão.
+
+    Parâmetros:
+    - con: conexão ativa do DuckDB
+    - profissoes: lista de strings com os nomes das profissões (ex: ['advogados', 'contadores'])
+    - cores: lista de cores na mesma ordem da lista de profissões (opcional)
+    """
+    if cores is None:
+        cores = ['#7f8c8d'] * len(profissoes)
+
+    dados = []
+    for i, profissao in enumerate(profissoes):
+        df = contar_profissionais(con, 'M', 'F', profissao)
+        coluna_total = f'total_{profissao}'
+        cor = cores[i] if i < len(cores) else '#7f8c8d'
+        dados.append((df, coluna_total, profissao.capitalize(), cor))
+
+    fig, axs = plt.subplots(2, 2, figsize=(14, 10))
+    fig.suptitle('Top Estados com mais profissionais por profissão', fontsize=16)
+
+    for ax, (df, coluna, titulo, cor) in zip(axs.flat, dados):
+        ax.barh(df['nome_estado'], df[coluna], color=cor)
+        ax.set_title(titulo)
+        ax.invert_yaxis()
+        for i, v in enumerate(df[coluna]):
+            ax.text(v + max(df[coluna]) * 0.01, i, f'{int(v):,}'.replace(',', '.'), va='center', fontsize=9)
+
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+    plt.show()
+
 
 def plotar_distribuicao_genero(df, profissoes, titulo, cor_legenda, genero_label):
     estados = df['nome_estado'].tolist()
@@ -115,28 +134,8 @@ def plotar_distribuicao_genero(df, profissoes, titulo, cor_legenda, genero_label
     plt.tight_layout()
     plt.show()
 
-def ordena_query_profissão(con, genero, profissao):
-	profissao_genero = con.execute(f"""
-		SELECT nome_estado, {genero}_{profissao} AS total
-		FROM df_estados_profissoes
-		ORDER BY total DESC
-		LIMIT 5
-		""").df()
-	return profissao_genero
+####################### FUNÇÃO PARA SALVAR ARQUIVOS JSON FINAIS #######################
 
-# Função para gerar gráfico horizontal com rótulos
-def plot_top5_profissao(df, coluna_valor, titulo, cor):
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.barh(df['nome_estado'], df[coluna_valor], color=cor)
-    ax.set_xlabel('Número de profissionais')
-    ax.set_title(titulo)
-
-    for i, v in enumerate(df[coluna_valor]):
-        ax.text(v + max(df[coluna_valor])*0.01, i, str(v), va='center', fontsize=10)
-
-    ax.invert_yaxis()  # Coloca o maior valor no topo
-    plt.tight_layout()
-    plt.show()
 
 def salvar_json(dataframes, pasta_resultados='../result'):
     # Caminho correto para a pasta result
